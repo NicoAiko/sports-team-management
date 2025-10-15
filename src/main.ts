f8dfaec510d3f511b99992671d36dd89e936a085
Nicole Linnemann <n.linnemann@nicoaiko.de>
2025-10-15 21:38:41 +0200

    adds postgres client

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { Application } from "@oak/oak";
 import { useRouter } from "./modules/router/router.ts";
 import "./modules/app-modules.ts";
+import { PostgresClient } from "./modules/postgres/client.ts";
 
 const app = new Application();
 const port = Number(Deno.env.get("HTTP_PORT") ?? 3000);
+
+Deno.addSignalListener("SIGINT", async () => {
+  await PostgresClient.destroy();
+
+  console.log("Shutting down...");
+  Deno.exit(0);
+});
+
+// Initialize Postgres Database connection
+await PostgresClient.init();
 
 // Register router and routes - modifies `app`
 useRouter(app);
