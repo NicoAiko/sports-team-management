9a0a6520f70259b94615f850ce4bb031f49b783e
Nicole Linnemann <n.linnemann@nicoaiko.de>
2025-10-18 17:21:24 +0200

    uses new DI

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,26 @@
-import { Application } from "@oak/oak";
-import { useRouter } from "./modules/router/router.ts";
-import "./modules/app-modules.ts";
-import { PostgresClient } from "./modules/postgres/client.ts";
+import { Application } from '@oak/oak';
+import { useRouter } from './core/router/router.ts';
+import { PostgresClient } from './core/postgres/client.ts';
+import './modules/app-modules.ts';
+import { inject } from './core/dependency-injection/inject.ts';
+import { LogService } from './core/logging/log.ts';
 
 const app = new Application();
-const port = Number(Deno.env.get("HTTP_PORT") ?? 3000);
+const port = Number(Deno.env.get('HTTP_PORT') ?? 3000);
 
-Deno.addSignalListener("SIGINT", async () => {
-  await PostgresClient.destroy();
+Deno.addSignalListener('SIGINT', async () => {
+  const postgresClient = inject(PostgresClient);
+  const logService = inject(LogService);
 
-  console.log("Shutting down...");
+  await postgresClient.destroy();
+
+  logService.debug('Shutting down...');
   Deno.exit(0);
 });
-
-// Initialize Postgres Database connection
-await PostgresClient.init();
 
 // Register router and routes - modifies `app`
 useRouter(app);
 
-console.log(`Running server on http://localhost:${port}`);
+inject(LogService).debug(`Running server on http://localhost:${port}`);
 
 app.listen({ port });
