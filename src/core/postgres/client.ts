82016142bb0a1761163d137f1594cd6db187cf06
Nicole Linnemann <n.linnemann@nicoaiko.de>
2025-10-19 16:33:26 +0200

    separates postgres client from setup module

--- src/core/postgres/client.ts
+++ src/core/postgres/client.ts
@@ -1,51 +1,12 @@
-import { Client } from '@db/postgres';
+import { type QueryArguments, type QueryObjectResult } from '@db/postgres';
 import { Injectable } from '../dependency-injection/decorators.ts';
-import type { OnInit } from '../dependency-injection/on-init.interface.ts';
-import { LogService } from '../logging/log.ts';
+import { PostgresModule } from './module.ts';
 
-@Injectable({ global: true, dependencies: [LogService] })
-export class PostgresClient implements OnInit {
-  private readonly client: Client;
+@Injectable({ global: true, dependencies: [PostgresModule] })
+export class PostgresClient {
+  constructor(private readonly postgres: PostgresModule) {}
 
-  constructor(private readonly logService: LogService) {
-    this.validateConfig();
-
-    this.client = new Client({
-      user: Deno.env.get('POSTGRES_USER'),
-      password: Deno.env.get('POSTGRES_PASSWORD'),
-      hostname: Deno.env.get('POSTGRES_HOST'),
-      port: Deno.env.get('POSTGRES_PORT') ?? 5432,
-      database: Deno.env.get('POSTGRES_DATABASE'),
-    });
-  }
-
-  public async onInit(): Promise<void> {
-    this.logService.debug('Connecting to postgres database...');
-    await this.client.connect();
-
-    this.logService.debug('Connected.');
-  }
-
-  public async destroy(): Promise<void> {
-    this.logService.debug('Gracefully disconnecting from postgres database');
-
-    await this.client.end();
-
-    this.logService.debug('Disconnected');
-  }
-
-  private validateConfig(): void | never {
-    if (
-      ![
-        'POSTGRES_USER',
-        'POSTGRES_PASSWORD',
-        'POSTGRES_HOST',
-        'POSTGRES_DATABASE',
-      ].every(Deno.env.has)
-    ) {
-      throw new Error(
-        'Postgres configuration is not set! Expected `POSTGRES_USER`, `POSTGRES_PASSWORD`, `POSTGRES_HOST`, `POSTGRES_DATABASE`',
-      );
-    }
+  public query<T = unknown>(query: string, args?: QueryArguments): Promise<QueryObjectResult<T>> {
+    return this.postgres.client.queryObject<T>(query, args);
   }
 }
---
+++ src/core/postgres/module.ts
@@ -0,0 +1,51 @@
+import { Client } from '@db/postgres';
+import { Injectable } from '../dependency-injection/decorators.ts';
+import type { OnInit } from '../dependency-injection/on-init.interface.ts';
+import { LogService } from '../logging/log.ts';
+
+@Injectable({ dependencies: [LogService] })
+export class PostgresModule implements OnInit {
+  public readonly client: Client;
+
+  constructor(private readonly logService: LogService) {
+    this.validateConfig();
+
+    this.client = new Client({
+      user: Deno.env.get('POSTGRES_USER'),
+      password: Deno.env.get('POSTGRES_PASSWORD'),
+      hostname: Deno.env.get('POSTGRES_HOST'),
+      port: Deno.env.get('POSTGRES_PORT') ?? 5432,
+      database: Deno.env.get('POSTGRES_DATABASE'),
+    });
+  }
+
+  public async onInit(): Promise<void> {
+    this.logService.debug('Connecting to postgres database...');
+    await this.client.connect();
+
+    this.logService.debug('Connected.');
+  }
+
+  public async destroy(): Promise<void> {
+    this.logService.debug('Gracefully disconnecting from postgres database');
+
+    await this.client.end();
+
+    this.logService.debug('Disconnected');
+  }
+
+  private validateConfig(): void | never {
+    if (
+      ![
+        'POSTGRES_USER',
+        'POSTGRES_PASSWORD',
+        'POSTGRES_HOST',
+        'POSTGRES_DATABASE',
+      ].every(Deno.env.has)
+    ) {
+      throw new Error(
+        'Postgres configuration is not set! Expected `POSTGRES_USER`, `POSTGRES_PASSWORD`, `POSTGRES_HOST`, `POSTGRES_DATABASE`',
+      );
+    }
+  }
+}
